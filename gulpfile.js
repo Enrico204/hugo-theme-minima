4ad0e9a53178d1dc795d54ceaa9a35aa3337d433
Daniel S. Reichenbach <daniel@kogitoapp.com>
2018-06-06 10:20:51 +0200

    Improved theme layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', [ 'sass' ]);
+gulp.task('default', ['sass']);
 
-gulp.task('sass', function () {
-    return gulp.src('sass/*.scss')
-    .pipe(sass({outputStyle: 'expanded', includePaths: ['node_modules', 'sass']}).on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('static/css/'));
-});
+gulp.task('sass', () =>
+    gulp
+        .src('src/sass/*.scss')
+        .pipe(sass({ outputStyle: 'expanded', includePaths: ['node_modules', 'src/sass'] }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('static/css/'))
+);
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/*.scss', ['sass']);
-});
+gulp.task('sass:watch', () =>
+    gulp.watch('./sass/*.scss', ['sass'])
+);
